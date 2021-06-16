947197879b448d2b86fdfc3c9af5ae10948ca0ef
Sulliman <johnatantc@hotmail.com>
2021-06-15 23:00:40 -0300

    Iniciando um novo projeto durante as aulas do módulo 4 de Front-End

---
+++ dragonball/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo.png'
+import './App.scss'
+
+
+function App() {
+  return (
+    <>
+      <div className="logoBar">
+        <img className="logoImg" src={logo} alt="Logo Dragonball Z"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
